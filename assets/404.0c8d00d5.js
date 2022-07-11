108c81dbabd337375ac5697d19fe22fc648e5a1b
tconns <tconns@users.noreply.github.com>
2022-07-11 08:10:06 +0000

    deploy: 0343ad9babc7b845f4121ec07ffe486b3821778a

---
+++ assets/404.0c8d00d5.js
@@ -0,0 +1,1 @@
+import"./index.5f133541.js";import{B as o}from"./button.2b6a0e80.js";import{R as s}from"./index.cf9e7473.js";import{k as t,m as e}from"./react-venders.15bc3e89.js";const p=()=>t(s,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:t(o,{type:"primary",onClick:()=>e.push("/"),children:"Back Home"})});export{p as default};
---
+++ assets/RootLayout.b9c43c1c.js
@@ -0,0 +1,1 @@
+import{k as o,F as a}from"./react-venders.15bc3e89.js";const s=({children:t})=>o(a,{children:t});export{s as default};
